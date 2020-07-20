7b3a93b8959ce837e76bb7fb4e8b09b0f6597607
WangFW <64408216+lucky-wfw@users.noreply.github.com>
2020-07-20 14:16:44 +0800

    Create 36_div3_tb.v

---
+++ 36_div3_tb.v
@@ -0,0 +1,23 @@
+//The testbench of div3
+//author:WangFW
+//date:2020-7-20
+
+module test_div3_tb();
+	reg clkin;
+	reg rst_n;
+	wire clkout;
+
+	initial
+		begin
+			clkin=0;
+			rst_n=1;
+			#10 rst_n=0;
+			#10 rst_n=1;
+		end
+
+	always #2 clkin<=~clkin;
+
+	test_div3 dut(.clkin(clkin),.rst_n(rst_n),.clkout(clkout));
+endmodule
+		
+			
